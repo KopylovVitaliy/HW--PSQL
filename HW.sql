89d24f81cdd0969fdee554cb6e8f1a48a3e88b26
PioneerX7 <vita-kopy@yandex.ru>
2023-04-07 12:56:22 +0300

    ДЗ по выборке данных

--- HW.sql
+++ HW.sql
@@ -1,9 +1,10 @@
-CREATE TABLE employee (
-                          id BIGSERIAL NOT NULL PRIMARY KEY,
-                          first_name VARCHAR(50) NOT NULL,
-                          last_name VARCHAR(50) NOT NULL ,
-                          gender VARCHAR(50) NOT NULL ,
-                          age INT NOT NULL
+CREATE TABLE employee
+(
+    id         BIGSERIAL   NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+    gender     VARCHAR(50) NOT NULL,
+    age        INT         NOT NULL
 );
 INSERT INTO employee(first_name, last_name, gender, age)
 VALUES ('Ivan', 'Petrov', 'Male', 20);
@@ -13,10 +14,14 @@ INSERT INTO employee(first_name, last_name, gender, age)
 VALUES ('Polina', 'Martin', 'FeMale', 22);
 SELECT *
 FROM employee;
-UPDATE employee SET first_name = 'Alisa' WHERE id = 1;
-UPDATE employee SET last_name = 'Stepanova' WHERE id = 1;
-UPDATE employee SET gender = 'FeMale' WHERE id = 1;
-UPDATE employee SET age = 23 WHERE id = 1;
+UPDATE employee SET first_name = 'Alisa'
+                  , last_name = 'Stepanova'
+                  , gender = 'FeMale'
+                  , age = 23
+                  WHERE id = 1;
+SELECT  *
+FROM employee;
+DELETE FROM employee WHERE id = 3;
 SELECT  *
 FROM employee;
 INSERT INTO employee(first_name, last_name, gender, age)
